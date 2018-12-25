0e2c81cdc87475a558fad703c8875f41eecef1e0
Marc <zunly@hotmail.com>
2018-12-25 15:34:52 +0800

    错误信息修改为中文

--- resources/views/users/all.blade.php
+++ resources/views/users/all.blade.php
@@ -2,4 +2,7 @@
 @section('title','全部用户')
 @section('content')
     {{--{{$user->name}}-{{$user->email}}--}}
+    @foreach($user->all() as $u)
+        <li>{{ $u->name }} : {{ $u->email }}</li>
+    @endforeach
 @stop

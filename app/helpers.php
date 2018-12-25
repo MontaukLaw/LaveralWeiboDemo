ed539630ccf14524cbeb21519c1bbd59bcc675d8
vagrant <vagrant@homestead>
2018-12-25 11:37:22 +0000

    changed db setting by following by heroku document

--- app/helpers.php
+++ app/helpers.php
@@ -12,19 +12,30 @@ function get_db_config()
         $url = parse_url(getenv("DATABASE_URL"));
 
         return $db_config = [
-            'connection' => 'pgsql',
+            'driver' => 'pgsql',
             'host' => $url["host"],
-            'database'  => substr($url["path"], 1),
-            'username'  => $url["user"],
-            'password'  => $url["pass"],
+            'port' => $url["port"],
+            'database' => ltrim($url["path"], "/"),
+            'username' => $url["user"],
+            'password' => $url["pass"],
+            'charset' => 'utf8',
+            'prefix' => '',
+            'schema' => 'public',
+            'sslmode' => 'require',
+//            'driver' => 'pgsql',
+//            'connection' => 'pgsql',
+//            'host' => $url["host"],
+//            'database'  => substr($url["path"], 1),
+//            'username'  => $url["user"],
+//            'password'  => $url["pass"],
         ];
     } else {
         return $db_config = [
             'connection' => env('DB_CONNECTION', 'mysql'),
             'host' => env('DB_HOST', 'localhost'),
-            'database'  => env('DB_DATABASE', 'homestead'),
-            'username'  => env('DB_USERNAME', 'homestead'),
-            'password'  => env('DB_PASSWORD', 'secret'),
+            'database' => env('DB_DATABASE', 'homestead'),
+            'username' => env('DB_USERNAME', 'homestead'),
+            'password' => env('DB_PASSWORD', 'secret'),
         ];
     }
 }

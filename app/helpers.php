24d879085ef21f62c224de06f05673e3ae6b940f
vagrant <vagrant@homestead>
2018-12-25 12:38:13 +0000

    changed db again 0.3

--- app/helpers.php
+++ app/helpers.php
@@ -12,16 +12,6 @@ function get_db_config()
         $url = parse_url(getenv("DATABASE_URL"));
 
         return $db_config = [
-//            'host' => $url["host"],
-//            'port' => $url["port"],
-//            'database' => ltrim($url["path"], "/"),
-//            'username' => $url["user"],
-//            'password' => $url["pass"],
-//            'charset' => 'utf8',
-//            'prefix' => '',
-//            'schema' => 'public',
-//            'sslmode' => 'require',
-//            'driver' => 'pgsql',
             'connection' => 'pgsql',
             'host' => $url["host"],
             'database' => substr($url["path"], 1),
